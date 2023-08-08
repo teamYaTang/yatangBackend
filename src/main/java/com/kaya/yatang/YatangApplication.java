a1f6ba7b255f0052608f7569e436a53e9eb3da38
Minju Kang <68880055+kayamz@users.noreply.github.com>
2023-08-09 02:11:07 +0900

    First commit

---
+++ src/main/java/com/kaya/yatang/YatangApplication.java
@@ -0,0 +1,13 @@
+package com.kaya.yatang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YatangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YatangApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kaya/yatang/YatangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaya.yatang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YatangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
