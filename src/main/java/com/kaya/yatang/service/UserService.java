0878aa9a4011ac2be6fba6af68323ec60eeae644
Minju Kang <68880055+kayamz@users.noreply.github.com>
2023-08-10 21:11:01 +0900

    Create UserService

---
+++ src/main/java/com/kaya/yatang/service/UserService.java
@@ -0,0 +1,71 @@
+package com.kaya.yatang.service;
+
+import com.kaya.yatang.dto.UserDTO;
+import com.kaya.yatang.entity.UserEntity;
+import com.kaya.yatang.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.apache.catalina.User;
+import org.springframework.stereotype.Service;
+
+import javax.swing.text.html.Option;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+    private final UserRepository userRepository;
+
+    public void save(UserDTO userDTO) {
+        UserEntity userEntity = UserEntity.toUserEntity(userDTO);
+        userRepository.save(userEntity);
+    }
+
+    public UserDTO login(UserDTO userDTO) {
+        Optional<UserEntity> byUserid = userRepository.findByUserid(userDTO.getUserid());
+
+        if (byUserid.isPresent()) {
+            UserEntity memberEntity = byUserid.get();
+            if (memberEntity.getUserpw().equals(userDTO.getUserpw())) {
+                // 비밀번호 일치
+                UserDTO dto = UserDTO.toUserDTO(memberEntity);
+                return dto;
+            } else {
+                // 비밀번호 불일치
+                return null;
+            }
+        } else {
+            return null;
+        }
+    }
+
+    public UserDTO findById(Long id) {
+        Optional<UserEntity> optionalUserEntity = userRepository.findById(id);
+        if (optionalUserEntity.isPresent()) {
+            return UserDTO.toUserDTO(optionalUserEntity.get());
+        } else {
+            return null;
+        }
+    }
+
+    public UserDTO nicknameForm(String myUserid) {
+        Optional<UserEntity> optionalUserEntity = userRepository.findByUserid(myUserid);
+        if (optionalUserEntity.isPresent()) {
+            return UserDTO.toUserDTO(optionalUserEntity.get());
+        } else {
+            return null;
+        }
+    }
+
+    public void nickname(UserDTO userDTO) {
+        userRepository.save(UserEntity.toUpdateUserEntity(userDTO));
+    }
+
+    public String useridCheck(String userid) {
+        Optional<UserEntity> byUserid = userRepository.findByUserid(userid);
+        if (byUserid.isPresent()) {
+            return null;
+        } else {
+            return "ok";
+        }
+    }
+}
