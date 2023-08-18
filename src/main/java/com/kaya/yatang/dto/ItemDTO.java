05a6cb9bbef9f93582391f232b4c64b79f112a3b
Minju Kang <68880055+kayamz@users.noreply.github.com>
2023-08-18 23:01:59 +0900

    Create ItemDTO

---
+++ src/main/java/com/kaya/yatang/dto/ItemDTO.java
@@ -0,0 +1,14 @@
+package com.kaya.yatang.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class ItemDTO {
+    private long id;
+    private String itemname;
+//    private String userid;
+}
