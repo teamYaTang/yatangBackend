8914398f4a65b11678b7dc6631b55b6eed21902f
Minju Kang <68880055+kayamz@users.noreply.github.com>
2023-09-13 18:01:34 +0900

    Update FridgeEntity.java

--- src/main/java/com/kaya/yatang/domain/fridge/FridgeEntity.java
+++ src/main/java/com/kaya/yatang/domain/fridge/FridgeEntity.java
@@ -1,4 +1,22 @@
 package com.kaya.yatang.domain.fridge;
 
+import com.kaya.yatang.entity.UserEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
 public class FridgeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @OneToOne
+    @JoinColumn(name = "userid")
+    UserEntity userEntity;
+
 }
